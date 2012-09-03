36ae7295b3f3431570cdd61a6c8794a4c58adb3a
Paulo Cesar <puelocesar@gmail.com>
2012-09-03 19:29:19 -0300

    initial

---
+++ app/InicioController.rb
@@ -0,0 +1,13 @@
+class InicioController < UIViewController #<UINavigationControllerDelegate, UIImagePickerControllerDelegate> {
+
+	attr_accessor :fade
+	attr_accessor :luz_poste
+  attr_accessor :imgPicker
+
+	def cameraClicado(sender)
+	end
+
+	def galeriaClicado(sender)
+	end
+
+end
